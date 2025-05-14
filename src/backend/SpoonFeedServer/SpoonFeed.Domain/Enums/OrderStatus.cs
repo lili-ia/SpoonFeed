f4426c0a23173da27861628a5d484206c947ecd0
SALOway <zhora.levchenkoko@gmail.com>
2025-05-14 18:52:54 +0300

    Reworked order

--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Enums/OrderStatus.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Enums/OrderStatus.cs
@@ -1,14 +1,40 @@
 namespace SpoonFeed.Domain.Enums;
 
+/// <summary>
+/// Represents the states of order delivery.
+/// </summary>
 public enum OrderStatus
 {
-    Pending,       
-    Confirmed,     
-    Preparing,     
-    ReadyForPickup, 
-    PickedUp,      
-    Delivering,    
-    Delivered,      
-    Cancelled,     
-    Failed         
+    /// <summary>
+    /// Order was created but hasn't yet been filled by the user.
+    /// </summary>
+    Preparing,
+    /// <summary>
+    /// Order has been created but payment is still pending.
+    /// </summary>
+    PendingPayment,
+    /// <summary>
+    /// User submitted the order, and now it require a courier to be delivered.
+    /// </summary>
+    WaitingForCourier,
+    /// <summary>
+    /// Courier is heading to the food facility.
+    /// </summary>
+    Delivering,
+    /// <summary>
+    /// Courier is waiting for the order item to be done.
+    /// </summary>
+    WaitingOrderItem,
+    /// <summary>
+    /// Courier has reached the destination and is waiting for the customer to take the order.
+    /// </summary>
+    WaitingCustomer,
+    /// <summary>
+    /// Order was successfully delivered.
+    /// </summary>
+    Delivered,
+    /// <summary>
+    /// Order was canceled.
+    /// </summary>
+    Cancelled
 }
--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Order.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Order.cs
@@ -1,28 +1,58 @@
+using System.ComponentModel.DataAnnotations;
 using SpoonFeed.Domain.Enums;
+using SpoonFeed.Domain.Owned;
 
 namespace SpoonFeed.Domain.Models;
 
+/// <summary>
+/// Represents the customer's order.
+/// </summary>
 public class Order : BaseEntity
 {
-    public DateTime AcceptedAt { get; set; }
+    private const OrderStatus DefaultStatus = OrderStatus.Preparing;
+    private const OrderPaymentMethod DefaultPaymentMethod = OrderPaymentMethod.Cash;
     
-    public OrderStatus OrderStatus { get; set; }
+    /// <summary>
+    /// Represents the timestamp when the order submitted by user.
+    /// </summary>
+    public DateTime? ReceivedAt { get; set; }
     
-    public string DeliveryAddress { get; set; }
+    /// <summary>
+    /// Represents the timestamp when the first courier started the delivery.
+    /// </summary>
+    public DateTime? AcceptedAt { get; set; }
     
-    public virtual IList<OrderPosition> OrderPositions { get; set; }
+    /// <summary>
+    /// Represents the timestamp when the order was delivered.
+    /// </summary>
+    public DateTime? DeliveredAt { get; set; }
     
-    public string PaymentMethod { get; set; }
+    /// <summary>
+    /// Represents the current status of order delivery.
+    /// </summary>
+    [Required(ErrorMessage = "Status is required.")]
+    public OrderStatus Status { get; set; } = DefaultStatus;
     
-    public DateTime ReceivedAt { get; set; }
+    /// <summary>
+    /// Represents the courier's final destination.
+    /// </summary>
+    [Required(ErrorMessage = "DeliveryAddress is required.")]
+    public Address DeliveryAddress { get; set; } = null!;
+
+    [Required(ErrorMessage = "PaymentMethod is required")]
+    public OrderPaymentMethod PaymentMethod { get; set; } = DefaultPaymentMethod;
     
-    public DateTime DeliveredAt { get; set; }
-    
+    [Required(ErrorMessage = "CustomerId is required.")]
     public Guid CustomerId { get; set; }
     
-    public virtual Customer Customer { get; set; }
+    /// <summary>
+    /// Represents the customer who owns the order.
+    /// </summary>
+    public virtual Customer Customer { get; set; } = null!;
     
-    public Guid CourierId { get; set; }
+    public Guid? CourierId { get; set; }
     
-    public virtual Courier Courier { get; set; }
+    public virtual Courier? Courier { get; set; }
+
+    public virtual IList<OrderPosition> OrderPositions { get; set; } = [];
 }
