dad941fd2d49ec6573c11414867b9fb1dd2d8f06
SALOway <zhora.levchenkoko@gmail.com>
2025-05-14 15:16:02 +0300

    Fixed logic. Added documentation

--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Courier.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Courier.cs
@@ -3,26 +3,45 @@ using SpoonFeed.Domain.Enums;
 
 namespace SpoonFeed.Domain.Models;
 
+/// <summary>
+/// Represents the courier and his account.
+/// </summary>
 public class Courier : UserIdentity
 {
-    public TransportType? TransportType { get; set; }
+    private const float DefaultDeliveryRange = 0.5f; 
     
+    /// <summary>
+    /// Represents the courier's type of transport.
+    /// Affects the max order weight.
+    /// </summary>
+    public TransportType TransportType { get; set; } = TransportType.None;
+
+    /// <summary>
+    /// Represents moneys the courier has earned by completing orders and which he can withdraw.
+    /// Calculated based on completed orders and withdrawn money.
+    /// </summary>
     [Required(ErrorMessage = "AccountBalance is required.")]
     [Range(0, double.MaxValue, ErrorMessage = "Account balance cannot be negative.")]
-    public double AccountBalance { get; set; }
+    public double AccountBalance { get; set; } = 0;
     
+    /// <summary>
+    /// Represents the current state of courier in the system, like offline, waiting order, delivering, etc.
+    /// Set by system's events, like start accepting, accept, get food, etc.
+    /// </summary>
     [Required(ErrorMessage = "CourierStatus is required.")]
-    public CourierStatus CourierStatus { get; set; }
-    
+    public CourierStatus CourierStatus { get; set; } = CourierStatus.Offline;
+
+    /// <summary>
+    /// Represents the distance in kilometers at which the courier can accept orders.
+    /// Max value depends on the courier's transport type.
+    /// </summary>
+    [Required(ErrorMessage = "DeliveryRange is required.")]
     [Range(0.5, 100, ErrorMessage = "Delivery range must be between 0.5 and 100 kilometers.")]
-    public double? DeliveryRange { get; set; } 
-    
-    [Range(0, 5, ErrorMessage = "Average rating must be between 0 and 5.")]
-    public double? AverageRating { get; set; }
-    
-    public virtual IList<CourierReview>? CourierReviews { get; set; }
-    
-    public virtual IList<Order>? Orders { get; set; }
-    
-    public virtual IList<Transaction>? Transactions { get; set; }
+    public float DeliveryRange { get; set; } = DefaultDeliveryRange;
+
+    public virtual IList<CourierReview> CourierReviews { get; set; } = [];
+
+    public virtual IList<Order> Orders { get; set; } = [];
+
+    public virtual IList<Transaction> Transactions { get; set; } = [];
 }
