54f605cc7197085e19fd55c9d8fc5747d0dc42ae
SALOway <zhora.levchenkoko@gmail.com>
2025-05-14 16:28:02 +0300

    Added missing required. Added documentation. Image.cs

--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Image.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Image.cs
@@ -1,8 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SpoonFeed.Domain.Models;
 
+/// <summary>
+/// Represents an image entity within the domain.
+/// </summary>
 public class Image : BaseEntity
 {
-    public string Title { get; set; }
+    /// <summary>
+    /// Represents the name of the image.
+    /// Often used as alt name in client apps.
+    /// </summary>
+    [Required(ErrorMessage = "Title is required.")]
+    public string Title { get; set; } = string.Empty;
     
-    public string Url { get; set; }
+    /// <summary>
+    /// Represents the resource location.
+    /// </summary>
+    [Required(ErrorMessage = "Url is required.")]
+    public string Url { get; set; } = string.Empty;
 }
