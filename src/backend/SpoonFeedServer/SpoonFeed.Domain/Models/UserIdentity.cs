d4192adbda7136f29786d5a71df7d7374eaa608a
SALOway <zhora.levchenkoko@gmail.com>
2025-05-14 15:25:25 +0300

    Fixed logic. Added documentation. UserIdentity.cs

--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/UserIdentity.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/UserIdentity.cs
@@ -2,25 +2,49 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SpoonFeed.Domain.Models;
 
+/// <summary>
+/// Represents user's credentials
+/// </summary>
 public abstract class UserIdentity : BaseEntity
 {
+    /// <summary>
+    /// Represent the user's email.
+    /// Allows sending emails to users.
+    /// </summary>
     [EmailAddress]
     [Required]
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
     
+    /// <summary>
+    /// Represent the user's first name.
+    /// Not required for customers.
+    /// </summary>
     [MinLength(3)]
     [MaxLength(30)]
     public string? FirstName { get; set; }
     
+    /// <summary>
+    /// Represent the user's last name.
+    /// Not required for customers.
+    /// </summary>
     [MinLength(3)]
     [MaxLength(30)]
     public string? LastName { get; set; }
     
+    /// <summary>
+    /// Represent the user's password, hashed for security reasons.
+    /// Used primary for comparison in auth service.
+    /// </summary>
     [MinLength(10)]
     [MaxLength(255)]
     [Required]
-    public string PasswordHash { get; set; }
+    public string PasswordHash { get; set; } = string.Empty;
     
+    /// <summary>
+    /// Represent the user's phone number.
+    /// Required by all the users for proper business logic. 
+    /// </summary>
     [MaxLength(20)]
-    public string? PhoneNumber { get; set; }
+    [Required]
+    public string PhoneNumber { get; set; } = string.Empty;
 }
