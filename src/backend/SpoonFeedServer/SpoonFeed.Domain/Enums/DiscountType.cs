086a43413acd7d9905f0adeb7d8756434dfe26fd
SALOway <zhora.levchenkoko@gmail.com>
2025-05-14 16:16:52 +0300

    Fixed logic. Added documentation. Discount.cs

--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Enums/DiscountType.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Enums/DiscountType.cs
@@ -1,9 +1,16 @@
 namespace SpoonFeed.Domain.Enums;
 
+/// <summary>
+/// Represents the type of discount value.
+/// </summary>
 public enum DiscountType
 {
-    Percentage,     
-    FixedAmount,   
-    FreeDelivery,   
-    BuyOneGetOne  
+    /// <summary>
+    /// The type of value is considered to be in normilized percents (e.g., 0.1 is 10%).
+    /// </summary>
+    Percentage,
+    /// <summary>
+    /// The type of value is considered to be scalar (e.g., 100 will be taken from the item's price).
+    /// </summary>
+    Scalar
 }
--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Discount.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Discount.cs
@@ -1,12 +1,40 @@
+using System.ComponentModel.DataAnnotations;
 using SpoonFeed.Domain.Enums;
 
 namespace SpoonFeed.Domain.Models;
 
+/// <summary>
+/// Represents a discount applied to a specific menu item within the system.
+/// </summary>
 public class Discount : BaseEntity
 {
+    private const DiscountType DefaultDiscountType = Enums.DiscountType.Percentage;
+    
+    /// <summary>
+    /// Represents the scalar value of discount.
+    /// </summary>
+    /// <remarks>
+    /// Percents are considered to be normalized (e.g., 0.1 is 10%, 2 is 200%, etc.).
+    /// </remarks>
+    [Required(ErrorMessage = "Value is required.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Value must be greater than 0.01.")]
     public double Value { get; set; }
     
-    public DiscountType DiscountType { get; set; }
+    /// <summary>
+    /// Represents the type of discount value.
+    /// </summary>
+    public DiscountType DiscountType { get; set; } = DefaultDiscountType;
     
+    /// <summary>
+    /// Represents the discount's invalidation time, after which it is not active.
+    /// </summary>
     public DateTime? InvalidAt { get; set; }
+
+    [Required(ErrorMessage = "MenuItemId is required.")]
+    public Guid MenuItemId { get; set; }
+
+    /// <summary>
+    /// Represents the item the discount is applied to. 
+    /// </summary>
+    public virtual MenuItem MenuItem { get; set; } = null!;
 }
