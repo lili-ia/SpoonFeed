eb527fe298c6b22e59a625beb1fadb201705ddc7
SALOway <zhora.levchenkoko@gmail.com>
2025-05-14 15:41:20 +0300

    Added defaults and documentation. Currency.cs

--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Currency.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/Currency.cs
@@ -2,15 +2,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SpoonFeed.Domain.Models;
 
+/// <summary>
+/// Represents currency-related data.
+/// Uses standard ISO 4217
+/// </summary>
 public class Currency : BaseEntity
 {
+    /// <summary>
+    /// Represents the fully qualified currency name, like "Euro", "Ukrainian Hryvnia", etc.
+    /// </summary>
     [Required(ErrorMessage = "Name is required.")]
     [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     
+    /// <summary>
+    /// Represents the standard currency code, like "UAH", "USD", etc.
+    /// </summary>
     [StringLength(10, ErrorMessage = "Code cannot exceed 10 characters.")]
-    public string Code { get; set; }
-    
+    public string Code { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Represents the symbol of the currency, such as "$", "NZ$", etc.
+    /// </summary>
     [StringLength(5, ErrorMessage = "Symbol cannot exceed 5 characters.")]
-    public string Symbol { get; set; }
+    public string Symbol { get; set; } = string.Empty;
 }
