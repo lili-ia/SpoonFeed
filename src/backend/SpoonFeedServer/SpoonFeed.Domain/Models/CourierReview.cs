8fb704793d3508bbd13d6bafcb12a22d3bbdc69d
SALOway <zhora.levchenkoko@gmail.com>
2025-05-14 15:36:35 +0300

    Added constants and documentation. CourierReview.cs

--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/CourierReview.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/CourierReview.cs
@@ -3,13 +3,24 @@ using SpoonFeed.Domain.Enums;
 
 namespace SpoonFeed.Domain.Models;
 
+/// <summary>
+/// Represents the customer's review on courier. 
+/// </summary>
 public class CourierReview : Review
 {
+    private const CourierReviewType DefaultReviewType = CourierReviewType.Other;
+    
     [Required(ErrorMessage = "CourierId is required.")]
     public Guid CourierId { get; set; }
     
-    public virtual Courier Courier { get; set; }
-    
-    [Required(ErrorMessage = "CourierReviewType is required.")]
-    public CourierReviewType CourierReviewType { get; set; }
+    /// <summary>
+    /// Represents the courier who is being reviewed.
+    /// </summary>
+    public virtual Courier Courier { get; set; } = null!;
+
+    /// <summary>
+    /// Represents the type of review, which describes its category.
+    /// </summary>
+    [Required(ErrorMessage = "ReviewType is required.")]
+    public CourierReviewType ReviewType { get; set; } = DefaultReviewType;
 }
