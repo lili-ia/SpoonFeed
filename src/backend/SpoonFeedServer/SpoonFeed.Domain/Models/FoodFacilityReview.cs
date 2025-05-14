50e554fd42d229b5ef6820635c259a017ab282c3
SALOway <zhora.levchenkoko@gmail.com>
2025-05-14 18:09:40 +0300

    Added documentation

--- src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/FoodFacilityReview.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Domain/Models/FoodFacilityReview.cs
@@ -2,12 +2,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SpoonFeed.Domain.Models;
 
+/// <summary>
+/// Represents customer's review on food facility.
+/// </summary>
 public class FoodFacilityReview : BaseEntity
 {
-    public Guid FoodFacilityId { get; set; }
-    
-    public virtual FoodFacility FoodFacility { get; set; }
-    
+    /// <summary>
+    /// Represents the food facility's score given by the customer. 
+    /// </summary>
+    [Required(ErrorMessage = "Score is required.")]
     [Range(1, 10)]
     public int Score { get; set; }
+    
+    [Required(ErrorMessage = "FoodFacilityId is required.")]
+    public Guid FoodFacilityId { get; set; }
+
+    /// <summary>
+    /// Represents the food facility which is being reviewed.
+    /// </summary>
+    public virtual FoodFacility FoodFacility { get; set; } = null!;
 }
