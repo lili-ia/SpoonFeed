d77729a11e7e98f9403a0b8cd93c9e9ab8715eb1
SALOway <zhora.levchenkoko@gmail.com>
2025-05-23 22:23:54 +0300

    Updated interface

--- src/backend/SpoonFeedServer/SpoonFeed.Application/Interfaces/IMenuService.cs
+++ src/backend/SpoonFeedServer/SpoonFeed.Application/Interfaces/IMenuService.cs
@@ -4,10 +4,10 @@ namespace SpoonFeed.Application.Interfaces;
 
 public interface IMenuService
 {
-    Task<Result<MenuItemResponse>> GetByIdAsync(Guid id);
-    Task<Result<IEnumerable<MenuItemResponse>>> GetByFacilityAsync(Guid foodFacilityId);
-    Task<Result<Guid>> CreateAsync(MenuItemCreateRequest request);
-    Task<Result<bool>> UpdateAsync(MenuItemUpdateRequest request);
-    Task<Result<bool>> DeleteAsync(Guid id);
-    Task<Result<bool>> ExistsAsync(Guid id);
+    Task<Result<MenuItemResponse>> GetByIdAsync(Guid id, CancellationToken ct);
+    Task<Result<IEnumerable<MenuItemResponse>>> GetByFacilityAsync(Guid foodFacilityId, CancellationToken ct);
+    Task<Result<Guid>> CreateAsync(MenuItemCreateRequest request, CancellationToken ct);
+    Task<Result<bool>> UpdateAsync(MenuItemUpdateRequest request, CancellationToken ct);
+    Task<Result<bool>> DeleteAsync(Guid id, CancellationToken ct);
+    Task<Result<bool>> ExistsAsync(Guid id, CancellationToken ct);
 }
